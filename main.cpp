7214f9baa3dd5d12edd8079651db768599171618
drakeaesoph <da00009@mix.wvu.edu>
2022-03-01 12:44:30 -0500

    kmp searching

--- main.cpp
+++ main.cpp
@@ -63,6 +63,76 @@ bool inverse(char* s)
     return good;
 }
 
+
+//input:
+//  s - string
+//  b - int array length(s) + 1
+void compute_border_array(char* s, int b[])
+{
+    int l = strlen(s);
+    b[0] = -1;
+    for(int i = 1; i <= l; i++) {
+        int t = b[i - 1];
+        while( t>= 0 && s[t] != s[i - 1]) {
+            t = b[t];
+        }
+        t++;
+        b[i] = t;
+    }
+}
+
+int find_length_kmp(char* r1, char* r2)
+{
+    // same as naive, except we have a "failure" function to speed up the shifts
+    // r1 is the "pattern" and r2 is the text
+    // we have the advantage here that we only have to search until the end of r2
+
+    //read lengths
+    int n = strlen(r1);
+    int m = strlen(r2);
+    int b[m + 1];
+    compute_border_array(r1, b);
+    int length = 0;
+    int i = 0, j = 0;
+
+    if(m != n)
+    {
+        cerr << "Somethin funky's goin down" << endl;
+    }
+
+    //naive approach: check if match
+    // if mismatch found:
+    //  shift R1 over (increase starting position of R2)
+    // check till end of string (R2)
+    bool found = false;
+
+    while(i < n - 15)
+    {
+        //r1 is pattern
+        //i is shift amount in r2 "text"
+        //j is current location
+        while(r2[i + j] != 0 && r1[j] == r2[i + j]){
+            j++;
+        }
+        if(r2[i + j] == 0)
+        {
+            found = true;
+            break;
+        }
+        i = i + (j - b[j]);
+        j = b[j] >= 0 ? b[j] : 0;
+    }
+    if(found)
+    {
+        //overlap region = (m - i)
+        //length = r1 + r2 - overlap
+        length = m + n - (m - i);
+    }
+
+    return length;
+
+}
+
 int find_length_naive(char* r1, char* r2)
 {
     //shift R1 right to find mathing suffix of R2
@@ -126,6 +196,7 @@ int find_length_naive(char* r1, char* r2)
 }
 
 int main(int argc, char** argv) {
+
     my_gzip r1, r2;
     char bufa[MAX_BUF], bufb[MAX_BUF];
     if(init(argc, argv, r1, r2))
@@ -137,7 +208,7 @@ int main(int argc, char** argv) {
             if(r1.eof()) break;
             inverse(bufb);
             //cout << "R1: " << bufa << endl << "R2: " << bufb << endl;
-            cout << find_length_naive(bufa, bufb) << endl;
+            cout << find_length_kmp(bufa, bufb) << endl;
             r1.readline(bufa, MAX_BUF); r2.readline(bufb, MAX_BUF);
             r1.readline(bufa, MAX_BUF); r2.readline(bufb, MAX_BUF);
         }
